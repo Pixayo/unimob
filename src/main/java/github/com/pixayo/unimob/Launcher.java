49fca33d38044c6794f2d42cbdf348d2221201d7
Pixayo <kaio.rodrigo729@gmail.com>
2026-04-04 15:47:13 -0300

    First commit

---
+++ src/main/java/github/com/pixayo/unimob/Launcher.java
@@ -0,0 +1,9 @@
+package github.com.pixayo.unimob;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module github.com.pixayo.unimob {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens github.com.pixayo.unimob to javafx.fxml;
+    exports github.com.pixayo.unimob;
+}
