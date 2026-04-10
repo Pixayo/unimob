29f07f95303c958f2640303a30080e9c67f53cb1
Pixayo <kaio.rodrigo729@gmail.com>
2026-04-10 19:17:40 -0300

    Change gitignore to exclude .idea folder and rename some classes

---
+++ src/main/java/github/com/pixayo/unimob/Application.java
@@ -0,0 +1,15 @@
+package github.com.pixayo.unimob;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        // carregar TelaMapa.fxml com FXMLLoader
+
+    }
+}
